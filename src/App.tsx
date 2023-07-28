0548828ea500dff8540c34a4374385132a23ffa7
Jean-Yves2 <sarakajean1@gmail.com>
2023-07-29 01:13:33 +0200

    add:Test Home page and set config route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+
 import "./App.css";
 
 function App() {
   return (
     <NextUIProvider>
-      <div className="containerAll">
-        <NavBar />
-        <Footer />
-      </div>
+      <Router>
+        <div className="containerAll">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
     </NextUIProvider>
   );
 }
-
 export default App;
