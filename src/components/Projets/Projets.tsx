35299b4f2c9243bb97a69acc86ef6558267fcf47
Jean-Yves2 <sarakajean1@gmail.com>
2023-08-29 17:25:44 +0200

    add:data

--- src/components/Projets/Projets.tsx
+++ src/components/Projets/Projets.tsx
@@ -1,33 +1,32 @@
 import "./Projets.scss";
+
+const projectsList = [
+  { name: "Opoll", date: "2021" },
+  { name: "TERRA", date: "2022" },
+  { name: "Opo", date: "2010" },
+  { name: "weather", date: "2020" }
+];
+
 function Projets() {
   return (
     <div className="projetsContainer">
       <h1>Mes Projets</h1>
       <div className="work-container">
-        <article className="work-right">
-         <span>
-          01/10
-         </span>
-         <div className="work-content">
-          <div className="work-title"></div>
-          <div className="work-date"></div>
-         </div>
+        {projectsList.map((project, index) => (
+          <article
+            key={index}
+            className={index % 2 === 0 ? "work-right" : "work-left"}
+          >
+            <span>{index+1}/{projectsList.length}</span>
+            <div className="work-content" style={{ backgroundImage: `url(src/assets/${project.name}.png)` }}>
+              <div className="work-title" data-project-name={project.name}></div>
+              <div className="work-date" data-project-date={project.date}></div>
+            </div>
           </article>
-
-          <article className="work-left">
-         <span>
-          01/10
-         </span>
-         <div className="work-content">
-          <div className="work-title"></div>
-          <div className="work-date"></div>
-         </div>
-          </article>
-
-
-          
+        ))}
       </div>
     </div>
   );
 }
+
 export default Projets;
