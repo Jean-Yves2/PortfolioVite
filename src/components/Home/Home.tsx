4f34c5a3f6d4e12f0145cebe2beb6c94a00210f3
Jean-Yves2 <sarakajean1@gmail.com>
2023-09-03 01:09:59 +0200

    💄 add: Section projects

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@nextui-org/react";
+import { Card, Text } from "@nextui-org/react";
 import "./Home.scss";
 import Particle from "../Particle/Particle";
 function Home() {
@@ -7,36 +7,49 @@ function Home() {
       <Particle></Particle>
       <div className="sectionContainer">
         <section className="sectionContainer__description">
-          <h1>Developpeur FullStack</h1>
-          <p>
-            Bonjour à tous ! Je suis un développeur Full Stack JS spécialisé
-            dans le développement web et mobile . En tant que développeur Full
-            Stack, je suis à l'aise à la fois en frontend et en backend. Je suis
-            capable de concevoir des interfaces utilisateur réactives et
-            intuitives en utilisant des frameworks tels que React et Vue.js. De
-            plus, je maîtrise également les langages de programmation côté
-            serveur tels que Node.js, ce qui me permet de développer des API
-            robustes et des services backend performants. Constamment à l'affût
-            des dernières tendances et des meilleures pratiques. Je suis
-            toujours prêt à relever de nouveaux défis et à apprendre de
-            nouvelles technologies pour améliorer mes compétences.Ma passion
-            pour le développement web et mobile ne se limite pas à une seule
-            plateforme ,ni a un language.Car avant d'être un développeur JS, je
-            suis un développeur.
-          </p>
+          <Card>
+            <Card.Body>
+              <Card.Header>
+                <Text>
+                  <h1>Developpeur FullStack</h1>
+                </Text>
+              </Card.Header>
+            </Card.Body>
+            <Text>
+              Bonjour à tous ! Je suis un développeur Full Stack JS spécialisé
+              dans le développement web et mobile . En tant que développeur Full
+              Stack, je suis à l'aise à la fois en frontend et en backend. Je
+              suis capable de concevoir des interfaces utilisateur réactives et
+              intuitives en utilisant des frameworks tels que React et Vue.js.
+              De plus, je maîtrise également les langages de programmation côté
+              serveur tels que Node.js, ce qui me permet de développer des API
+              robustes et des services backend performants. Constamment à
+              l'affût des dernières tendances et des meilleures pratiques. Je
+              suis toujours prêt à relever de nouveaux défis et à apprendre de
+              nouvelles technologies pour améliorer mes compétences.Ma passion
+              pour le développement web et mobile ne se limite pas à une seule
+              plateforme ,ni a un language.Car avant d'être un développeur JS,
+              je suis un développeur.
+            </Text>
+          </Card>
         </section>
         <section className="sectionContainer__card">
           <Card css={{ w: "50%" }}>
-            
             <Card.Image
               src="src\assets\me.jpg"
-              width="100%"
-              height="100%"
               objectFit="cover"
               alt="Card image background"
             />
           </Card>
         </section>
+
+        <div className="sectionContainer__projectSection">
+          <div className="sectionContainer__projectSection__white">
+            <h2 className="sectionContainer__projectSection__white-projet">
+              Projets
+            </h2>
+          </div>
+        </div>
       </div>
     </div>
   );
