893fa2b53fa3dee510bc9642d1a09f13fe57160b
Jean-Yves2 <sarakajean1@gmail.com>
2023-09-06 01:02:53 +0200

    add: project on home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,13 @@ import { Card, Text } from "@nextui-org/react";
 import "./Home.scss";
 import Particle from "../Particle/Particle";
 function Home() {
+
+  const projectsList = [
+    { name: "Opoll", date: "2023" ,techno:"REACT - MATERIEL UI - SOKET.IO - MARIADB - JWT" },
+    { name: "RainCheck", date: "2023" ,techno:"JAVASCRIPT" },
+    { name: "FilmCollection", date: "2023" ,techno:"NODEJS - EXPRESS - POSTGRESQL - EJS" }
+  ];
+
   return (
     <div className="homeContainer">
       <Particle></Particle>
@@ -11,11 +18,13 @@ function Home() {
             <Card.Body>
               <Card.Header>
                 <Text>
-                  <h1>Developpeur FullStack</h1>
+                  <h1 className="sectionContainer__description--title">
+                    Developpeur FullStack Js
+                  </h1>
                 </Text>
               </Card.Header>
             </Card.Body>
-            <Text>
+            <Text className="sectionContainer__description--text">
               Bonjour à tous ! Je suis un développeur Full Stack JS spécialisé
               dans le développement web et mobile . En tant que développeur Full
               Stack, je suis à l'aise à la fois en frontend et en backend. Je
@@ -42,13 +51,26 @@ function Home() {
             />
           </Card>
         </section>
-
         <div className="sectionContainer__projectSection">
           <div className="sectionContainer__projectSection__white">
             <h2 className="sectionContainer__projectSection__white-projet">
-              Projets
+              Expériences
             </h2>
           </div>
+        </div>
+        <div className="sectionContainer__selectedProject">
+        {projectsList.map((project, index) => (
+          <article className="sectionContainer__selectedProject__article" 
+          key={index}>
+            <section className="sectionContainer__selectedProject__article--description">
+              <h2>{project.name}</h2>
+              <p> {project.date}</p>
+              <p>{project.techno}</p>
+            </section>
+            <section className="sectionContainer__selectedProject__article--image">
+              <img src={`src/assets/${project.name}.png`} alt="Opoll Project" />
+            </section>
+          </article>))}
         </div>
       </div>
     </div>
